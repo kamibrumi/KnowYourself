346f3db5e8b2ac2dad4bdf0aa062a10bb5ea9a2e
CameliaBrumi <alcbrum8@gmail.com>
2017-07-18 21:38:09 +0200

    am stabilit conexiunea la internet si am calculat average temperature
    -mai ramane sa pun toate astea pe debug 3,
    -sa fac sa functioneze mai bine notificatia
    -sa scriu intr-un fisier average temperature

--- Weather/app/src/main/java/yukami/weather/MainActivity.java
+++ Weather/app/src/main/java/yukami/weather/MainActivity.java
@@ -13,45 +13,91 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+
 public class MainActivity extends AppCompatActivity {
+    private static String URL = "http://api.openweathermap.org/data/2.5/forecast?q=Barcelona,es&APPID=afbef7bdcea5f0feb4b7e97fe6b57aba";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WeatherHttpClient whp = new WeatherHttpClient();
-        String weatherData = "";
-        if (isNetworkAvailable()) weatherData = whp.getWeatherData("Barcelona,es");
-        System.out.println("VREMEA PE AZI" + weatherData);
-    }
-
-    private boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager
-                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-    }
-     /*
-    @Override
-    public void onStart() {
-        super.onStart();
-
-
-
-
-        // We create out JSONObject from the data
-        JSONObject jObj;
+        String weatherData = null;
         try {
-            jObj = new JSONObject(((new WeatherHttpClient()).getWeatherData("Barcelona,es")));
-            jObj.getJSONArray("list");
-            JSONArray arr = jObj.getJSONArray("list");
-            Log.i("INSIDE THE LIST:    ", arr.toString());
+            weatherData = new getURLData().execute(URL).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        if (weatherData == null) System.out.println("weather data = NULL!!!!!");
+        else System.out.println(weatherData);
+
+        JSONObject obj = null;
+        try {
+            obj = new JSONObject(weatherData);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        */
+
+        JSONArray arr = null;
+        try {
+            arr = obj.getJSONArray("list");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Date date=new Date();
+        SimpleDateFormat dayFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dt = dayFormat.format(date); // Start date
+        System.out.println("DATA DE ASTAZI ESTE: " + dt);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar c = Calendar.getInstance();
+        try {
+            c.setTime(sdf.parse(dt));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        c.add(Calendar.DATE, 1);  // number of days to add
+        dt = sdf.format(c.getTime());  // dt is now the new date
+        System.out.println("DATA DE MAINE ESTE : " + dt);
 
 
+        double average_temp = 0; //TODO: change this to float point number (this version gives a number with exponent). you have to get the string and the convert it to float number.
+        int nrOfHours = 0;
+        String dt_compare = null;
+
+        for (int i = 0; i < arr.length(); i++) {
+            try {
+
+                dt_compare = arr.getJSONObject(i).getString("dt_txt");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            System.out.println(dt_compare);
+            if (dt_compare.contains(dt)) {
+                System.out.println(dt_compare);
+                ++nrOfHours;
+                System.out.println("COMPTADOR" + nrOfHours);
+
+                JSONObject main = null;
+                try {
+                    main = arr.getJSONObject(i).getJSONObject("main");
+                    average_temp += main.getDouble("temp");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
+        average_temp = average_temp/nrOfHours;
+        System.out.println("AVERAGE TEMPERATURE OF TOMORROW IS : " + average_temp);
+    }
 
 }
--- Weather/app/src/main/java/yukami/weather/getURLData.java
+++ Weather/app/src/main/java/yukami/weather/getURLData.java
@@ -2,36 +2,96 @@ package yukami.weather;
 
 import android.os.AsyncTask;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class getURLData extends AsyncTask<String, Integer, String> {
 
+    private static String BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?q=";
+    private static String IULIS_KEY = "&APPID=afbef7bdcea5f0feb4b7e97fe6b57aba";
+    private static String IMG_URL = "http://openweathermap.org/img/w/";
+
     @Override
-    protected String doInBackground(String... params) {
-        String line;
+    protected String doInBackground(String... location) {
+        HttpURLConnection con = null ;
+        InputStream is = null;
+
         try {
-            WeatherHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(params[0]);
+            con = (HttpURLConnection) ( new URL(BASE_URL + location + IULIS_KEY)).openConnection();
+            System.out.println("------------S-A FACUT OPENCONNEC");
+            con.setRequestMethod("GET");
+            System.out.println("con.setRequestMethod(\"GET\");");
+            con.setDoInput(true);
+            System.out.println("con.setDoInput(true);");
 
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            HttpEntity httpEntity = httpResponse.getEntity();
-            line = EntityUtils.toString(httpEntity);
+            con.setDoOutput(true);
+            System.out.println("con.setDoOutput(true);");
+            con.connect();
+            System.out.println("con.connect();");
+            //chivato
+            System.out.println("-----------S-A CONNECTAT");
+            // Let's read the response
+            StringBuffer buffer = new StringBuffer();
+            is = con.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+            while ( (line = br.readLine()) != null )
+                buffer.append(line + "rn");
+            if (line == null) System.out.println("-----------LINE IS NULL!!!!!!");
+            else System.out.println("------------------LINE = " + line);
 
-        } catch (UnsupportedEncodingException e) {
-            line = "<results status=\"error\"><msg>Can't connect to server</msg></results>";
-        } catch (MalformedURLException e) {
-            line = "<results status=\"error\"><msg>Can't connect to server</msg></results>";
-        } catch (IOException e) {
-            line = "<results status=\"error\"><msg>Can't connect to server</msg></results>";
+            is.close();
+            con.disconnect();
+            System.out.println("-----------------S-A DESCONNECTAT");
+            return buffer.toString();
         }
-        return line;
+        catch(Throwable t) {
+            t.printStackTrace();
+        }
+        finally {
+            try { is.close(); } catch(Throwable t) {}
+            try { con.disconnect(); } catch(Throwable t) {}
+        }
+
+        return null;
+
     }
 
-    @Override
-    protected void onPostExecute(String result) {
-        super.onPostExecute(result);
+    //[I] The following code is not needed, at least not in the initial steps
+    public byte[] getImage(String code) {
+        HttpURLConnection con = null ;
+        InputStream is = null;
+        try {
+            con = (HttpURLConnection) ( new URL(IMG_URL + code)).openConnection();
+            con.setRequestMethod("GET");
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.connect();
+
+            // Let's read the response
+            is = con.getInputStream();
+            byte[] buffer = new byte[1024];
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            while ( is.read(buffer) != -1)
+                baos.write(buffer);
+
+            return baos.toByteArray();
+        }
+        catch(Throwable t) {
+            t.printStackTrace();
+        }
+        finally {
+            try { is.close(); } catch(Throwable t) {}
+            try { con.disconnect(); } catch(Throwable t) {}
+        }
+
+        return null;
+
     }
 
 }
