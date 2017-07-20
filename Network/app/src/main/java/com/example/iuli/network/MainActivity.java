ea46739ecac692b086a07ca928647bd158c757ed
CameliaBrumi <alcbrum8@gmail.com>
2017-07-20 16:34:32 +0200

    Am stabilit o conexiune intre calculator si telefon
    de moment pot trimite string-ul care este in fisierul unde salvam datele
    Next Goal: trimite un fisier din telefon la calculator si salveaza-l acolo

---
+++ Network/app/src/main/java/com/example/iuli/network/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.iuli.network;
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class MainActivity extends Activity
+{
+    private TCPClient mTcpClient;
+    TextView serverResponse;
+    TextView clientTv;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        writeToFile("hey there!", this);
+        Button send = (Button)findViewById(R.id.send_button);
+        serverResponse = (TextView) findViewById(R.id.serverResponse);
+        clientTv = (TextView) findViewById(R.id.clientMessage);
+
+        // connect to the server
+        new connectTask().execute("");
+    }
+
+    public void sendMessage(View v) {
+        //String message = editText.getText().toString();
+        String message = readFromFile();
+
+        //add the text in the arrayList
+        clientTv.setText("c: " + message);
+
+        //sends the message to the server
+        if (mTcpClient != null) {
+            mTcpClient.sendMessage(message);
+        }
+    }
+
+    public class connectTask extends AsyncTask<String,String,TCPClient> {
+
+        @Override
+        protected TCPClient doInBackground(String... message) {
+
+            //we create a TCPClient object and
+            mTcpClient = new TCPClient(new TCPClient.OnMessageReceived() {
+                @Override
+                //here the messageReceived method is implemented
+                public void messageReceived(String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+            });
+            mTcpClient.run();
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            serverResponse.setText(values[0]);
+            //arrayList.add(values[0]);
+            //we can add the message received from server to a text view
+
+        }
+    }
+    private void writeToFile(String data, Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter;
+            outputStreamWriter = new OutputStreamWriter(context.openFileOutput("data1.txt", Context.MODE_APPEND));
+            outputStreamWriter.write(data + '\n');
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+    private String readFromFile() {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = this.openFileInput("data1.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+}
