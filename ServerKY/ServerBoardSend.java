057d622ca856cbdefc58cff48500b570be86c9bf
CameliaBrumi <alcbrum8@gmail.com>
2017-08-01 16:12:00 +0200

    Am adaugat scriptul de conectare intre server si tel
    De asemenea:
    - TCPServerSend.java
    - ServerBoardSend.java
    
    Este serverul de trimitere de date la mobil (zi buna, zi rea)
    
    Am reusit sa fac conexiunea cu telefonul, mai ramane sa fac ca telefonul sa primeasca mesajul si sa-l puna pe pantalla.

---
+++ ServerKY/ServerBoardSend.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.net.ServerSocket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerBoardSend {
+
+    public static void main(String[] args) {
+	
+	//we start the server
+
+        TCPServer mServer = new TCPServer(new TCPServer.OnMessageReceived() {
+                    @Override
+                    //this method declared in the interface from TCPServer class is implemented here
+                    //this method is actually a callback method, because it will run every time when it will be called from
+                    //TCPServer class (at while)
+                    public void messageReceived(String message) {}});
+                mServer.start();
+		
+		// send the message to the client
+      		mServer.sendMessage(args[args.length - 1]); //el porcentaje de good
+    }
+}
---
+++ ServerKY/TCPServerSend.java
@@ -0,0 +1,105 @@
+import javax.swing.*;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * The class extends the Thread class so we can receive and send messages at the same time
+ */
+public class TCPServerSend extends Thread {
+
+    public static final int SERVERPORT = 55160;
+    private boolean running = false;
+    private PrintWriter mOut;
+    private OnMessageReceived messageListener;
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+    /**
+     * Constructor of the class
+     * @param messageListener listens for the messages
+     */
+    public TCPServerSend(OnMessageReceived messageListener) {
+        this.messageListener = messageListener;
+    }
+
+    /**
+     * Method to send the messages from server to client
+     * @param message the message sent by the server
+     */
+    public void sendMessage(String message){
+        if (mOut != null && !mOut.checkError()) {
+            mOut.println(message);
+            mOut.flush();
+        }
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+        running = true;
+
+        try {
+            System.out.println("S: Connecting...");
+
+            //create a server socket. A server socket waits for requests to come in over the network.
+            ServerSocket serverSocket = new ServerSocket(SERVERPORT);
+
+            //create client socket... the method accept() listens for a connection to be made to this socket and accepts it.
+            Socket client = serverSocket.accept();
+            System.out.println("S: Receiving...");
+
+            try {
+
+                //sends the message to the client
+                mOut = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
+		/*
+                //read the message received from client
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+                //in this while we wait to receive messages from client (it's an infinite loop)
+                //this while it's like a listener for messages
+                /*while (running) {
+                    String message = in.readLine();
+
+                    if (message != null && messageListener != null) {
+                        //call the method messageReceived from ServerBoard class
+                        messageListener.messageReceived(message);
+                    }
+                } 
+		String message = in.readLine();
+
+                if (message != null && messageListener != null) {
+                    //call the method messageReceived from ServerBoard class
+                    messageListener.messageReceived(message);
+                } */
+
+            } catch (Exception e) {
+                System.out.println("S: Error");
+                e.printStackTrace();
+            } finally {
+                client.close();
+                System.out.println("S: Done.");
+            }
+
+        } catch (Exception e) {
+            System.out.println("S: Error");
+            e.printStackTrace();
+        }
+
+    }
+
+    //Declare the interface. The method messageReceived(String message) will must be implemented in the ServerBoard
+    //class at on startServer button click
+    public interface OnMessageReceived {
+        public void messageReceived(String message);
+    }
+
+
+
+}
