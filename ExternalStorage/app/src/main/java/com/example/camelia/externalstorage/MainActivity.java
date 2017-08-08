8b7e80e72b8e3710dd337bf4f460b20e440486f8
CameliaBrumi <alcbrum8@gmail.com>
2017-08-08 18:14:06 +0200

    ExternalStorage: am reusit sa scriu/citesc un fisier extern!
    Ramane sa vedem ce se intampla daca nu exista fisierul, probabil am sa folosesc file.isEmpty();
    
    Please enter the commit message for your changes. Lines starting

--- ExternalStorage/app/src/main/java/com/example/camelia/externalstorage/MainActivity.java
+++ ExternalStorage/app/src/main/java/com/example/camelia/externalstorage/MainActivity.java
@@ -11,11 +11,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,55 +42,76 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
-        getDir(data);
-        if (!file.exists())  getDir(data);
-        tv = (TextView) findViewById(R.id.tv);
-        checkExternalMedia();
-        writeToSDFile();
-        readRaw();
-        // Get the directory for the user's public pictures directory.
-        File file = new File(String.valueOf(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES)));
-        if (!file.mkdirs()) {
-            Log.e(LOG_TAG, "Directory not created");
-        } */
-        //createExternalStoragePublicPicture();
-        //writeToFile();
-
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onResume(){
         super.onResume();
-        writeToFile();
-    }
-
-    private void writeToFile() {
-
         String root = Environment.getExternalStorageDirectory().toString();
         File myDir = new File(root + "/docs");
         myDir.mkdirs();
 
         File file = new File (myDir, "data.txt");
+
+        writeToFile(file, "hellooooo");
+        String content = "";
+        try {
+            content = readFromFile1(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("THE CONTENT OF THE FILE IS: " + content);
+    }
+
+    public String readFromFile1(File file) throws IOException {
+        //get InputStream of a file
+        InputStream is = new FileInputStream(file);
+        String strContent;
+
+                /*
+                 * There are several way to convert InputStream to String. First is using
+                 * BufferedReader as given below.
+                 */
+
+        //Create BufferedReader object
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(is));
+        StringBuffer sbfFileContents = new StringBuffer();
+        String line = null;
+
+        //read file line by line
+        while( (line = bReader.readLine()) != null){
+            sbfFileContents.append(line);
+        }
+
+        //finally convert StringBuffer object to String!
+        strContent = sbfFileContents.toString();
+
+                /*
+                 * Second and one liner approach is to use Scanner class. This is only supported
+                 * in Java 1.5 and higher version.
+                 */
+
+        //strContent = new Scanner(is).useDelimiter("\\A").next();
+        return strContent;
+
+    }
+
+
+    private void writeToFile(File file, String data) {
+        /*
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/docs");
+        myDir.mkdirs();
+
+        File file = new File (myDir, "data.txt"); */
         if (file.exists ()) file.delete ();
         try {
-            //File inputFile = new File("farrago.txt");
-            //File outputFile = new File("data.txt");
-            System.out.println("inainte de posibila erroare");
-            //FileInputStream fis = new FileInputStream(inputFile);
             FileOutputStream fos = new FileOutputStream(file);
-            int c;
-            String str = "Here is Cami";
-            byte[] strb = str.getBytes();
-            /*
-            while ((c = fis.read()) != -1) {
-                fos.write(c);
-            } */
+            byte[] strb = data.getBytes();
             for(int i = 0; i < strb.length; ++i) {
                 fos.write(strb[i]);
             }
-
-            //fis.close();
             fos.close();
         } catch (FileNotFoundException e) {
             System.err.println("FileStreamsTest: " + e);
