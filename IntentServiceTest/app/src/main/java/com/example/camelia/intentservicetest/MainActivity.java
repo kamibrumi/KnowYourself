8b07cfba0480684c1b575486cde358b41830e022
CameliaBrumi <alcbrum8@gmail.com>
2017-08-18 16:12:44 +0200

    Am facut un nou mic proiect ca sa probez faza cu intentService.
    Ma intor la master ca sa fac fisierul xs.txt extern.

---
+++ IntentServiceTest/app/src/main/java/com/example/camelia/intentservicetest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.camelia.intentservicetest;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startIntentService(View v) {
+        Intent intent = new Intent(this, MyIntentService.class);
+        startService(intent);
+    }
+}
---
+++ IntentServiceTest/app/src/main/java/com/example/camelia/intentservicetest/MyIntentService.java
@@ -0,0 +1,62 @@
+package com.example.camelia.intentservicetest;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class MyIntentService extends IntentService {
+    String TAG = "TAG";
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionFoo(Context context, String param1, String param2) {}
+
+    /**
+     * Starts this service to perform action Baz with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionBaz(Context context, String param1, String param2) {}
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.v(TAG, "In onHandleIntent. Message will be printed after 10sec");
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionFoo(String param1, String param2) {
+        // TODO: Handle action Foo
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionBaz(String param1, String param2) {
+        // TODO: Handle action Baz
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
