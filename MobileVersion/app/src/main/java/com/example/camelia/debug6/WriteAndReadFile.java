c5f2bbb6dd776d007c24fb697313e165c609569c
CameliaBrumi <alcbrum8@gmail.com>
2019-12-16 17:19:20 -0500

    Somehow missed the mobile version

---
+++ MobileVersion/app/src/main/java/com/example/camelia/debug6/WriteAndReadFile.java
@@ -0,0 +1,125 @@
+package com.example.camelia.debug6;
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * Created by camelia on 24/07/18.
+ */
+
+public class WriteAndReadFile {
+    public static String dataDirectoryName = "/Know Yourself";
+
+    public static void writeToExternalFile(String filename, String data, Boolean append) {
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + dataDirectoryName);
+        myDir.mkdirs();
+
+        File file = new File (myDir, filename);
+        try {
+            FileOutputStream fos = new FileOutputStream(file, append); //true because we append
+            byte[] strb = data.getBytes();
+            for(int i = 0; i < strb.length; ++i) {
+                fos.write(strb[i]);
+            }
+            fos.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("FileStreamsTest: " + e);
+        } catch (IOException e) {
+            System.err.println("FileStreamsTest: " + e);
+        }
+    }
+
+    public static void writeToInternalFile(String fileName, String data, Context context, int MODE) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(fileName, MODE)); // inainte era: Context.MODE_PRIVATE
+            outputStreamWriter.write(data + '\n');
+            outputStreamWriter.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    public static String readFromExternalFile(String filename) throws IOException {
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + dataDirectoryName);
+        myDir.mkdirs();
+
+        File file = new File (myDir, filename);
+        //get InputStream of a file
+        InputStream is = new FileInputStream(file);
+        String strContent;
+
+                /*
+                 * There are several way to convert InputStream to String. First is using
+                 * BufferedReader as given below.
+                 */
+
+        //Create BufferedReader object
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(is));
+        StringBuffer sbfFileContents = new StringBuffer();
+        String line = null;
+
+        //read file line by line
+        while( (line = bReader.readLine()) != null){
+            sbfFileContents.append(line);
+        }
+
+        //finally convert StringBuffer object to String!
+        strContent = sbfFileContents.toString();
+
+                /*
+                 * Second and one liner approach is to use Scanner class. This is only supported
+                 * in Java 1.5 and higher version.
+                 */
+
+        //strContent = new Scanner(is).useDelimiter("\\A").next();
+        return strContent;
+
+    }
+
+
+    public static String readFromInternalFile(String fileName, Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput(fileName);
+
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+
+
+}
